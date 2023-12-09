b725f4a7656ebc8d41dcb43304ec97ca44c599c4
neil-hughes <neil.hughes@techreturners.com>
2023-12-09 06:34:03 +0000

    aoc: 2023 Day 9

--- puzzles/day-9/day-9-b.ts
+++ puzzles/day-9/day-9-b.ts
@@ -1,9 +1,39 @@
-import { readData } from '../../shared.ts';
+import { parseNumberList, readData, sum } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day9b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const lines = await readData(dataPath);
+
+  const sequences = lines.map((l) => parseNumberList(l, ' '));
+
+  const prevValues = sequences.map((s) => predictPreviousValue(s));
+
+  return sum(prevValues);
+}
+
+function predictNextValue(seq: number[]): number {
+  if (seq.every((n) => n === 0)) {
+    return 0;
+  } else {
+    return seq[seq.length - 1] + predictNextValue(findDifferences(seq));
+  }
+}
+
+function predictPreviousValue(seq: number[]): number {
+  if (seq.every((n) => n === 0)) {
+    return 0;
+  } else {
+    return seq[0] - predictPreviousValue(findDifferences(seq));
+  }
+}
+
+function findDifferences(sequence: number[]): number[] {
+  const differences: number[] = [];
+  for (let i = 0; i < sequence.length - 1; i++) {
+    differences.push(sequence[i + 1] - sequence[i]);
+  }
+
+  return differences;
 }
 
 const answer = await day9b();
