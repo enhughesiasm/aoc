82f8d0fbc23a252105b3448021da15783642da0a
neil-hughes <neil.hughes@techreturners.com>
2024-01-09 18:24:01 +0000

    aoc: 2023 Day 15

--- puzzles/day-15/day-15-a.ts
+++ puzzles/day-15/day-15-a.ts
@@ -1,9 +1,29 @@
-import { readData } from '../../shared.ts';
+import { readData, sum } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day15a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const steps = parseSteps(data[0]);
+
+  return sum(steps.map((s) => calculateHASH(s)));
+}
+
+function parseSteps(input: string) {
+  return input.split(',');
+}
+
+function calculateHASH(input: string): number {
+  let currentValue = 0;
+
+  for (const char of input) {
+    currentValue += char.charCodeAt(0);
+
+    currentValue *= 17;
+
+    currentValue = currentValue % 256;
+  }
+
+  return currentValue;
 }
 
 const answer = await day15a();
