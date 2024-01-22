3aeaa948ea581a85afa3dae3cf31a0c9b55976a4
neil-hughes <neil.hughes@techreturners.com>
2024-01-22 07:52:07 +0000

    aoc: 2023 Day 1 - brought code in from standalone solution before the repo

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -1,9 +1,12 @@
-import { readData } from '../../shared.ts';
+import { readData } from '../../lib/shared.ts';
 import chalk from 'chalk';
 
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  return data.reduce((accumulator, line) => {
+    const digits = line.match(/\d/gm);
+    return accumulator + Number(digits[0] + digits.at(-1));
+  }, 0);
 }
 
 const answer = await day1a();
--- puzzles/day-1/day-1-b.ts
+++ puzzles/day-1/day-1-b.ts
@@ -1,9 +1,28 @@
-import { readData } from '../../shared.ts';
+import { readData } from '../../lib/shared.ts';
 import chalk from 'chalk';
 
 export async function day1b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const transformed = data.map((l) => transformLine(l));
+
+  return transformed.reduce((accumulator, line) => {
+    const digits = line.match(/\d/gm);
+    return accumulator + Number(digits[0] + digits.at(-1));
+  }, 0);
+}
+
+function transformLine(l: string): string {
+  return l
+    .replaceAll('one', 'one1one')
+    .replaceAll('two', 'two2two')
+    .replaceAll('three', 'three3three')
+    .replaceAll('four', 'four4four')
+    .replaceAll('five', 'five5five')
+    .replaceAll('six', 'six6six')
+    .replaceAll('seven', 'seven7seven')
+    .replaceAll('eight', 'eight8eight')
+    .replaceAll('nine', 'nine9nine');
 }
 
 const answer = await day1b();
