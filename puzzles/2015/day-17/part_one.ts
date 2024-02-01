460b2f1920f5106e3b32a7274bd9e201623067e4
neil-hughes <neil.hughes@techreturners.com>
2024-02-01 22:04:44 +0000

    aoc: 2015 Day 17

--- puzzles/2015/day-17/part_one.ts
+++ puzzles/2015/day-17/part_one.ts
@@ -3,5 +3,36 @@ import { readData } from '../../../lib/shared.ts';
 export async function solve(dataPath?: string) {
   const data = await readData(dataPath);
 
-  return 0;
+  const containers = data.map((x) => Number.parseInt(x));
+
+  const MAX_LITRES = 150;
+  const combinations = findContainerCombinations(containers, MAX_LITRES);
+
+  // console.log(combinations);
+
+  return combinations.length;
+}
+function findContainerCombinations(containers, total) {
+  function findCombinations(index, remainingTotal, currentCombination) {
+    if (remainingTotal === 0) {
+      result.push([...currentCombination]);
+      return;
+    }
+
+    for (let i = index; i < containers.length; i++) {
+      if (remainingTotal - containers[i] >= 0) {
+        currentCombination.push(containers[i]);
+        findCombinations(
+          i + 1,
+          remainingTotal - containers[i],
+          currentCombination
+        );
+        currentCombination.pop();
+      }
+    }
+  }
+
+  const result = [];
+  findCombinations(0, total, []);
+  return result;
 }
--- puzzles/2015/day-17/part_two.ts
+++ puzzles/2015/day-17/part_two.ts
@@ -1,7 +1,41 @@
-import { readData } from '../../../lib/shared.ts';
+import { min, readData } from '../../../lib/shared.ts';
 
 export async function solve(dataPath?: string) {
   const data = await readData(dataPath);
 
-  return 0;
+  const containers = data.map((x) => Number.parseInt(x));
+
+  const MAX_LITRES = 150;
+  const combinations = findContainerCombinations(containers, MAX_LITRES);
+
+  const combinationSizes = combinations.map((c) => c.length);
+
+  const minimumSize = min(combinationSizes);
+
+  return combinations.filter((c) => c.length === minimumSize).length;
+}
+
+function findContainerCombinations(containers, total): number[][] {
+  function findCombinations(index, remainingTotal, currentCombination) {
+    if (remainingTotal === 0) {
+      result.push([...currentCombination]);
+      return;
+    }
+
+    for (let i = index; i < containers.length; i++) {
+      if (remainingTotal - containers[i] >= 0) {
+        currentCombination.push(containers[i]);
+        findCombinations(
+          i + 1,
+          remainingTotal - containers[i],
+          currentCombination
+        );
+        currentCombination.pop();
+      }
+    }
+  }
+
+  const result = [];
+  findCombinations(0, total, []);
+  return result;
 }
