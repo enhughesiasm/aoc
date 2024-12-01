5bbbb8c2c0587b404c158f6b1541ca530388bf03
neil-hughes <neil.hughes@techreturners.com>
2024-12-01 06:12:11 +0000

    wip: 2016 Day 10

--- puzzles/2016/day-10/part_one.ts
+++ puzzles/2016/day-10/part_one.ts
@@ -1,7 +1,49 @@
-import { readData } from '../../../lib/shared.ts';
+import { extractNumbers, readData } from '../../../lib/shared.ts';
+
+type Instruction =
+  | {
+      kind: 'VALUE';
+      value: number;
+      bot: number;
+    }
+  | {
+      kind: 'GIVE';
+      botIndex: number;
+      lowType: 'OUTPUT' | 'BOT';
+      lowIndex: number;
+      highType: 'OUTPUT' | 'BOT';
+      highIndex: number;
+    };
 
 export async function solve(dataPath?: string) {
   const data = await readData(dataPath);
 
+  const inst = data.map(parseInstruction);
+
   return 0;
+}
+
+function parseInstruction(s: string): Instruction {
+  const nums = extractNumbers(s);
+
+  if (s.startsWith('value')) {
+    return {
+      kind: 'VALUE',
+      value: nums[0],
+      bot: nums[1],
+    };
+  } else {
+    const givePart = s.split('gives')[1];
+    const giveNums = extractNumbers(givePart);
+    const giveSplit = givePart.split(' and ');
+
+    return {
+      kind: 'GIVE',
+      botIndex: nums[0],
+      lowType: giveSplit[0].includes('output') ? 'OUTPUT' : 'BOT',
+      lowIndex: giveNums[0],
+      highType: giveSplit[1].includes('output') ? 'OUTPUT' : 'BOT',
+      highIndex: giveNums[1],
+    };
+  }
 }
