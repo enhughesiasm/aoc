b74d33af9ad261eb5bbaa0c9619f3f4fa703515c
neil-hughes <neil.hughes@techreturners.com>
2024-12-02 06:52:58 +0000

    feat: 2024 Day 2

--- puzzles/2024/day-2/part_one.ts
+++ puzzles/2024/day-2/part_one.ts
@@ -1,7 +1,31 @@
-import { readData } from '../../../lib/shared.ts';
+import { numericOnly, readData } from '../../../lib/shared.ts';
 
 export async function solve(dataPath?: string) {
   const data = await readData(dataPath);
 
-  return 0;
+  const levels = data.map((l) => l.split(' ').map((x) => parseInt(x)));
+
+  const differences = levels.map((l) => calculateDifferences(l));
+
+  const areSafe = differences.map((d) => isSafe(d));
+  return areSafe.filter((d) => d === true).length;
+}
+
+function calculateDifferences(levels: number[]): number[] {
+  const differences: number[] = [];
+  for (let i = 0; i < levels.length - 1; i++) {
+    differences.push(levels[i + 1] - levels[i]);
+  }
+  return differences;
+}
+
+function isSafe(differences: number[]): boolean {
+  if (differences.some((x) => x === 0)) return false;
+
+  if (differences.every((x) => x > 0) || differences.every((x) => x < 0)) {
+    if (differences.every((x) => x >= -3 && x <= 3)) {
+      return true;
+    }
+  }
+  return false;
 }
--- puzzles/2024/day-2/part_two.ts
+++ puzzles/2024/day-2/part_two.ts
@@ -1,7 +1,38 @@
-import { readData } from '../../../lib/shared.ts';
+import { numericOnly, readData } from '../../../lib/shared.ts';
 
 export async function solve(dataPath?: string) {
   const data = await readData(dataPath);
 
-  return 0;
+  const levels = data.map((l) => l.split(' ').map((x) => parseInt(x)));
+
+  const areReportsSafe = levels.map((d) => tryProblemDampener(d));
+  return areReportsSafe.filter((d) => d === true).length;
+}
+
+function calculateDifferences(levels: number[]): number[] {
+  const differences: number[] = [];
+  for (let i = 0; i < levels.length - 1; i++) {
+    differences.push(levels[i + 1] - levels[i]);
+  }
+  return differences;
+}
+
+function tryProblemDampener(levels: number[]): boolean {
+  if (isSafe(calculateDifferences(levels))) return true;
+
+  for (let i = 0; i < levels.length; i++) {
+    const filteredLevels = levels.filter((_, j) => j !== i);
+    if (isSafe(calculateDifferences(filteredLevels))) return true;
+  }
+
+  return false;
+}
+
+function isSafe(differences: number[]): boolean {
+  if (differences.every((x) => x > 0) || differences.every((x) => x < 0)) {
+    if (differences.every((x) => x >= -3 && x <= 3)) {
+      return true;
+    }
+  }
+  return false;
 }
